fc41a5d59e27b8e9d6fe48b36d2eee91b9b1ff6f
Mribrahimkhan360 <mribrahimkhan360@gmail.com>
2024-11-25 10:10:02 +0600

    8:25 PM on Sunday, November 24, 2024

--- 19.vite-project - managing formsubmit/src/App.jsx
+++ 19.vite-project - managing formsubmit/src/App.jsx
@@ -8,16 +8,20 @@ const App = () => {
  
 
  const change=()=>{
-  myHead.current.innerText="Hello";
+  myHead.current.innerHTML=`<ul>
+  <li>list a</li>
+  <li>list b</li>
+  </ul>`;
  }
+ // eslint-disable-next-line no-undef
+ //disable
   return (
     <div>
-      <h1 ref={myHead}>Form Valiadition</h1>
+      <h1 ref={(h1)=>myHead=h1}>Form Valiadition</h1>
       <form onSubmit={formFun}>
         <input type="text" placeholder="Enter text" />
         <button type="submit" onClick={change}>Submit</button>
       </form>
-    
     </div>
   );
 };
